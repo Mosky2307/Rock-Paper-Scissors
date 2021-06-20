d7e09b8de7bb93aa901e8032924c0096bf15b837
mosky <smartmosky@gmail.com>
2021-06-19 23:21:27 -0400

    added message for all 3 player win scenarios

--- rps.js
+++ rps.js
@@ -5,3 +5,11 @@ let a = "rock"
 let b ="paper"
 let c = "scissors"
 let rps = [a, b, c]
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection === a && computerSelection === c) ||
+    ((playerSelection === b && computerSelection) === a) ||
+    (playerSelection === c && computerSelection === b))
+    return alert("You win!") 
+};
+const playerSelection = c;
+const computerSelection = computerPlay();
