62f835ee8e3746524d0d26ac63f400840ad6f348
mosky <smartmosky@gmail.com>
2021-06-21 00:15:48 -0400

    added results for player losing and draw

--- rps.js
+++ rps.js
@@ -9,7 +9,10 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection === a && computerSelection === c) ||
     ((playerSelection === b && computerSelection) === a) ||
     (playerSelection === c && computerSelection === b))
-    return alert("You win!") 
+    {return alert("You win!")} else if
+    (playerSelection === computerSelection) 
+    {return alert("Draw!")} else
+    return alert("You lose!")
 };
 const playerSelection = c;
-const computerSelection = computerPlay();
+const computerSelection = computerPlay(); 
