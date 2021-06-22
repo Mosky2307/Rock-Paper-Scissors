f6d7056464650c232800df7ba909e900f6bfdfd1
mosky <smartmosky@gmail.com>
2021-06-22 13:57:28 -0400

    added 5 round game and put player and computer selection in playRound function so they can change each time

--- rps.js
+++ rps.js
@@ -1,18 +1,26 @@
 function computerPlay() {
     return rps[Math.floor(Math.random() * rps.length)];
-}
-let a = "rock"
-let b ="paper"
-let c = "scissors"
-let rps = [a, b, c]
-function playRound(playerSelection, computerSelection) {
+};
+let a = "rock";
+let b ="paper";
+let c = "scissors";
+let rps = [a, b, c];
+function playRound() {
+    computerPlay()
+    playerSelection = c
+    computerSelection = computerPlay()
     if ((playerSelection === a && computerSelection === c) ||
-    ((playerSelection === b && computerSelection) === a) ||
+    (playerSelection === b && computerSelection === a) ||
     (playerSelection === c && computerSelection === b))
-    {return alert("You win!")} else if
+    {return alert("You win!")}  else if
     (playerSelection === computerSelection) 
     {return alert("Draw!")} else
     return alert("You lose!")
 };
-const playerSelection = c;
-const computerSelection = computerPlay(); 
+function game() {
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+};
