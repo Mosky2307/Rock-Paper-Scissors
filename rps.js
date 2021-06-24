586384b3b08e2457520aac54be1f73e42c834de7
mosky <smartmosky@gmail.com>
2021-06-24 00:11:57 -0400

    Made player input case-insensitive, made tally for player wins

--- rps.js
+++ rps.js
@@ -6,21 +6,29 @@ let b ="paper";
 let c = "scissors";
 let rps = [a, b, c];
 function playRound() {
-    computerPlay()
-    playerSelection = c
-    computerSelection = computerPlay()
-    if ((playerSelection === a && computerSelection === c) ||
-    (playerSelection === b && computerSelection === a) ||
-    (playerSelection === c && computerSelection === b))
-    {return alert("You win!")}  else if
-    (playerSelection === computerSelection) 
-    {return alert("Draw!")} else
-    return alert("You lose!")
+    computerPlay();
+    playerSelection = prompt("Please choose: Rock, paper or scissors?");
+    computerSelection = computerPlay();
+    if ((playerSelection.toLowerCase() === a && computerSelection === c) ||
+    (playerSelection.toLowerCase() === b && computerSelection === a) ||
+    (playerSelection.toLowerCase() === c && computerSelection === b))
+    {alert("You win!"); }  else if
+    (playerSelection.toLowerCase() === computerSelection) 
+    { alert("Draw!"); } else
+     alert("You lose!");
 };
 function game() {
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-};
+    let playerWins = 0;
+   for (let i=0; i<5; i++) {
+       playRound(); if ((playerSelection.toLowerCase() === a && computerSelection === c) ||
+       (playerSelection.toLowerCase() === b && computerSelection === a) ||
+       (playerSelection.toLowerCase() === c && computerSelection === b)) {
+           playerWins++
+       }
+    };
+  if (playerWins >= 3) {
+         alert("You reign supreme!")} else
+        { alert("Better luck next time!")
+    }
+}
+game()
