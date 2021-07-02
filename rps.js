cb36ddec5e84f67097ec298a14f05ab6101cbbbb
mosky <smartmosky@gmail.com>
2021-07-02 16:31:03 -0400

    Added message for winning or losing 5 rounds, disabled buttons when that happens, created play again button that resets everything

--- rps.js
+++ rps.js
@@ -25,6 +25,23 @@ function playRound(playerSelection) {
     losses.textContent = "Losses : " + (computerWins);
     lastGame.textContent = "You Lost!";
     }
+    if (playerWins === 5) {
+      finalResults.textContent = "You reign supreme!";
+      document.getElementById("rockBtn").disabled = true;
+      document.getElementById("paperBtn").disabled = true;
+      document.getElementById("scsrsBtn").disabled = true;
+      flexbox.appendChild(playAgain);
+    } else if  (computerWins === 5) {
+      finalResults.textContent = "Better luck next time!"
+      document.getElementById("rockBtn").disabled = true;
+      document.getElementById("paperBtn").disabled = true;
+      document.getElementById("scsrsBtn").disabled = true;
+      flexbox.appendChild(playAgain);
+      
+    }
+    else {
+
+    }
 };
 
 
@@ -53,5 +70,27 @@ const flexbox = document.querySelector('#flexbox');
 const lastGame = document.createElement('div');
 lastGame.classList.add("lastGame");
 lastGame.textContent = "";
-flexbox.appendChild(lastGame)
+flexbox.appendChild(lastGame);
+const finalResults = document.createElement('div');
+finalResults.classList.add("finalResults");
+finalResults.textContent = "";
+flexbox.appendChild(finalResults);
+const buttons = document.querySelector('#buttons');
+const playAgain = document.createElement('button');
+playAgain.classList.add("playAgain");
+playAgain.textContent = "Play again!"
+playAgain.addEventListener('click', () => {
+  playerWins = 0;
+  computerWins = 0;
+  wins.textContent = "Wins : " + (playerWins);
+  losses.textContent = "Losses : " + (computerWins);
+  lastGame.textContent = "";
+  finalResults.textContent = "";
+  document.getElementById("rockBtn").disabled = false;
+      document.getElementById("paperBtn").disabled = false;
+      document.getElementById("scsrsBtn").disabled = false;
+});
+
+
+
 
